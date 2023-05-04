a3aecd6c2a15774031bd52ede313501735cfdac1
winnieSikinyi <winniesikinyi@gmail.com>
2023-05-04 14:08:11 +0300

    yx

--- assignment.js
+++ assignment.js
@@ -4,8 +4,11 @@ class Vehicle{
         this.model = model;
         this.year = year;
         this.isAvailable =isAvailable;
-        this.toggleAvailability= toggleAvailability
 
+    }
+
+    toggleAvailability(){
+        this.isAvailable = this.isAvailable;
     }
 }
 let vehicle = new Vehicle (`Toyota`,`Camry`,2020,`True`,`false`);
@@ -20,30 +23,53 @@ class Rental extends Vehicle{
         this.rentendDate = rentendDate;
         this.rentDue = rentDue;
     }
+    duration(){
+        const oneDay = 4 * 60 
+        const startDate = new Date(this.rentalStartDate);
+        const endDate = new Date(this.rentalEndDate);
+
+    }
 }
 let rentedCar = new Rental (`Nissan`,`Sunny`,2016,`True`,`false`
-,`Alex`,`12.3.2023`,`12.4.2023`,`7.4.2023`);
+,`Alex`,`12/3/2023`,`12/4/2023`,`7/4/2023`);
 
 console.log({rentedCar});
 
-class Questions{
-    constructor(text,options,correctAnswers){
-        this.text = text;
-        this.options = options;
-        this.correctAnswers = correctAnswers;
-    }
-    checkAnswer(){
-        console.log(`correct`);
-    }
-}
-let questions = new Questions (`How are you today`,`it is there`,`it is not there`,`yes`);
-console.log({Questions});
-Questions(questions)
 
-class Quiz extends Questions (){
-    constructor(text,options,correctAnswers ){
-        super(text,options,correctAnswers)
-
+class Questions {
+    constructor(text, options, correctAnswer) {
+      this.text = text;
+      this.options = options;
+      this.correctAnswer = correctAnswer;
     }
+  
+    checkAnswer(userAnswer) {
+      return expectedAnswer === this.correctAnswer;
+    }
+  }
 
-}
+
+  class Quiz extends Questions {
+    constructor() {
+      this.questions = [];
+      this.currentQuestionIndex = 0;
+      this.score = 0;
+    }
+  
+    addQuestion(question) {
+      this.questions.push(question);
+    }
+  
+    nextQuestion() {
+      this.currentQuestionIndex++;
+    }
+  
+    submitAnswer(userAnswer) {
+      const currentQuestion = this.questions[this.currentQuestionIndex];
+      if (currentQuestion.checkAnswer(userAnswer)) {
+        this.score++;
+      }
+    }
+  }
+  let askedquestion = new Quiz (`WHERE`,`are you`)
+  
